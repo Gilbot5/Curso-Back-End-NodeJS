9e2a9804418ed54ca25d509bcd34fa806fad8654
Gilbot5 <gilbertordaz@gmail.com>
2025-11-28 12:15:59 -0300

    agregando la collection de postman

--- ENTREGA/src/middlewares/authentication.js
+++ ENTREGA/src/middlewares/authentication.js
@@ -8,6 +8,10 @@ const secret_key = process.env.JWT_SECRET_KEY || 'secret';
 // Middleware para verificar el token JWT 
 export const authentication = (req, res, next) => {
 
+    if(!req.headers['authorization']){
+        return res.status(401).json({ message: unauthorized });
+    }
+    
     const token = req.headers['authorization'].split(" ")[1];
     if (!token) return res.status(401).json({ message: unauthorized });
     jwt.verify(token, secret_key, (err) => {
